638ae366c867104461e0da86b11019befa2c0401
Srikanth Vudharapu <sidharthsid74659@gmail.com>
2021-10-04 00:21:01 +0530

    updated styles

--- public/script.js
+++ public/script.js
@@ -1,6 +1,26 @@
+// @ts-nocheck
 const theme = document.querySelector('.theme input[type="checkbox"]')
 const color__list = document.querySelector('.color__list');
+const color__input = document.querySelector('.color__input');
+const btn__add = document.querySelector('.btn--add');
 
+color__input.addEventListener('input', (e) => {
+    if (e.target.value.length >= 3) {
+        if (CSS.supports('color', e.target.value)) {
+            color__input.classList.remove('error')
+            color__input.classList.add('success')
+            btn__add.disabled = false;
+        } else {
+            color__input.classList.remove('success')
+            color__input.classList.add('error')
+            btn__add.disabled = true;
+        }
+    } else {
+        color__input.classList.remove('success')
+        color__input.classList.remove('error')
+        btn__add.disabled = true;
+    }
+})
 theme.addEventListener('change', () => {
     document.documentElement.classList.toggle('dark-theme')
 })
@@ -11,7 +31,7 @@ function copyColor(e) {
     // check if the user clicked one of the children 
     if (!e.target.closest("label")) return;
     const id = e.target.children[0].dataset.index;
-    
+
     //Why: e.target.nextSibling Doesn't work ?
     const copyEl = document.querySelector(`#color${id}`);
     copyEl.select();
@@ -23,4 +43,28 @@ function copyColor(e) {
 
     // color_list.classList.add('copy');
     // setTimeout(() => color_list.classList.remove('copy'), 250);
+}
+document.querySelector('.btn--add').addEventListener("click", addColor);	//avoid duplicate colors
+
+async function addColor(event) {
+    event.preventDefault();
+
+    let newColor = color__input.value.toLowerCase();
+
+    console.log(CSS.supports('color', newColor))
+
+    // newColor = (`#${newColor}`.includes('##')) ? tinycolor(newColor).toHexString() : newColor.toString();
+
+    // await fetch('/db/create', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({ newColor: newColor })
+    // })
+
+    //update the color onto Display
+    // populateList();
+
+    // form.reset();
 }
